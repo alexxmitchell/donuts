3e966f22154a397296722ff019462aae2a52c810
Alexx Mitchell <alexxmitchell25@gmail.com>
2018-05-02 17:00:15 -0500

    can drop toppings

--- src/components/Toppings/Droptop/Droptop.js
+++ src/components/Toppings/Droptop/Droptop.js
@@ -12,160 +12,146 @@ import cakepbdriz from "../../images/cakewpbdrizzle.png";
 import cakecaramel from "../../images/cakewcaramelicing.png";
 import cakerasp from "../../images/cakewraspicing.png";
 import cakechoc from "../../images/cakewchocicing.png";
+import { ifError } from "assert";
 
 class Droptop extends Component {
   constructor(props) {
     super(props);
+
+    // this.state = {
+    //   toppings: []
+    // }
   }
   render() {
     let image = "";
     let icing = "";
     let toppings = [];
-    let topping1 = "";
-    let topping2 = "";
-    let topping3 = "";
-    let topping4 = "";
-    let topping5 = "";
-    let topping6 = "";
-    let topping7 = "";
-    let topping8 = "";
-    let topping9 = "";
-    let topping10 = "";
-    let topping11 = "";
-    let topping12 = "";
 
-    if (this.props.currentDonut && this.props.currentDonut.kind === "Cake") {
-      switch (this.props.atop) {
-        case 1:
-          image = cake;
-          console.log("peanuts");
-          break;
-        case 2:
-          image = cake;
-          console.log("pecans");
-          break;
-        case 3:
-          image = cake;
-          console.log("bacon");
-          break;
-        case 4:
-          image = cake;
-          console.log("gold");
-          break;
-        case 5:
-          image = cake;
-          console.log("strawberries");
-          break;
-        case 6:
-          image = cake;
-          console.log("bananas");
-          break;
-        case 7:
-          image = cake;
-          console.log("sprinkles");
-          break;
-        case 8:
-          image = cake;
-          toppings.push(cakechoc);
-          console.log("chocolate icing");
-          break;
-        case 9:
-          image = cake;
-          toppings.push(cakecaramel);
-          console.log("caramel icing");
-          break;
-        case 10:
-          image = cake;
-          toppings.push(cakerasp);
-          console.log("raspberry icing");
-          break;
-        case 11:
-          image = cake;
-          toppings.push(cakechocdriz);
-          console.log("chocolate drizzle");
-          break;
-        case 12:
-          image = cake;
-          toppings.push(cakepbdriz);
-          console.log("pb drizzle");
-          break;
-        default:
-          image = cake;
-          console.log("default", this.props.atop);
-      }
-    } else if (
-      this.props.currentDonut &&
-      this.props.currentDonut.kind === "Chocolate"
-    ) {
-      switch (this.props.atop) {
-        case 1:
-          image = choc;
-          topping1 = cake;
-          console.log("peanuts");
-          break;
-        case 2:
-          image = choc;
-          console.log("pecans");
-          break;
-        case 3:
-          image = choc;
-          console.log("bacon");
-          break;
-        case 4:
-          image = choc;
-          console.log("gold");
-          break;
-        case 5:
-          image = choc;
-          console.log("strawberries");
-          break;
-        case 6:
-          image = choc;
-          console.log("bananas");
-          break;
-        case 7:
-          image = choc;
-          console.log("sprinkles");
-          break;
-        case 8:
-          image = choc;
-          toppings.push(chocwchoc);
-          console.log("chocolate icing");
-          break;
-        case 9:
-          image = choc;
-          toppings.push(chocwcaramel);
-          console.log("caramel icing");
-          break;
-        case 10:
-          image = choc;
-          toppings.push(chocrasp);
-          console.log("raspberry icing");
-          break;
-        case 11:
-          image = choc;
-          console.log("chocolate drizzle");
-          break;
-        case 12:
-          image = choc;
-          console.log("pb drizzle");
-          break;
-        default:
-          image = choc;
-          console.log("default");
-      }
+    if (this.props.currentDonut.kind === "Cake") {
+      image = cake;
+    } else if (this.props.currentDonut.kind === "Chocolate") {
+      image = choc;
     }
+
+    let displayToppings = this.props.currentDoTop.map((e, i) => {
+      if (this.props.currentDonut && this.props.currentDonut.kind === "Cake") {
+        switch (e) {
+          case 1:
+            return <img key={i} className="stacking size" src={choc} />;
+            console.log("peanuts");
+            break;
+          case 2:
+            return <img key={i} className="stacking size" src={choc} />;
+            console.log("pecans");
+            break;
+          case 3:
+            return <img key={i} className="stacking size" src={bacon} />;
+            console.log("bacon");
+            break;
+          case 4:
+            return <img key={i} className="stacking size" src={choc} />;
+            console.log("gold");
+            break;
+          case 5:
+            return <img key={i} className="stacking size" src={choc} />;
+            console.log("strawberries");
+            break;
+          case 6:
+            return <img key={i} className="stacking size" src={choc} />;
+            console.log("bananas");
+            break;
+          case 7:
+            return <img key={i} className="stacking size" src={choc} />;
+            console.log("sprinkles");
+            break;
+          case 8:
+            return <img key={i} className="stacking size" src={cakechoc} />;
+            console.log("chocolate icing");
+            break;
+          case 9:
+            return <img key={i} className="stacking size" src={cakecaramel} />;
+            console.log("caramel icing");
+            break;
+          case 10:
+            return <img key={i} className="stacking size" src={cakerasp} />;
+            console.log("raspberry icing");
+            break;
+          case 11:
+            return <img key={i} className="stacking size" src={cakechocdriz} />;
+            console.log("chocolate drizzle");
+            break;
+          case 12:
+            return <img key={i} className="stacking size" src={cakepbdriz} />;
+            console.log("pb drizzle");
+            break;
+          default:
+            console.log("default", this.props.atop);
+        }
+      } else if (
+        this.props.currentDonut &&
+        this.props.currentDonut.kind === "Chocolate"
+      ) {
+        switch (e) {
+          case 1:
+            return <img key={i} className="stacking size" src={cake} />;
+            console.log("peanuts");
+            break;
+          case 2:
+            return <img key={i} className="stacking size" src={cake} />;
+            console.log("pecans");
+            break;
+          case 3:
+            return <img key={i} className="stacking size" src={bacon} />;
+            console.log("bacon");
+            break;
+          case 4:
+            return <img key={i} className="stacking size" src={cake} />;
+            console.log("gold");
+            break;
+          case 5:
+            return <img key={i} className="stacking size" src={cake} />;
+            console.log("strawberries");
+            break;
+          case 6:
+            return <img key={i} className="stacking size" src={cake} />;
+            console.log("bananas");
+            break;
+          case 7:
+            return <img key={i} className="stacking size" src={cake} />;
+            console.log("sprinkles");
+            break;
+          case 8:
+            return <img key={i} className="stacking size" src={chocwchoc} />;
+
+            console.log("chocolate icing");
+            break;
+          case 9:
+            return <img key={i} className="stacking size" src={chocwcaramel} />;
+
+            console.log("caramel icing");
+            break;
+          case 10:
+            return <img key={i} className="stacking size" src={chocrasp} />;
+            console.log("raspberry icing");
+            break;
+          case 11:
+            return <img key={i} className="stacking size" src={choc} />;
+            console.log("chocolate drizzle");
+            break;
+          case 12:
+            return <img key={i} className="stacking size" src={choc} />;
+            console.log("pb drizzle");
+            break;
+          default:
+            console.log("default");
+        }
+      }
+    });
     console.log(this.props);
 
-    let displayToppings = this.props.currentToppings.map(e => {
-      return <img key={e} className="stacking size" src={e} />;
-    });
     return (
       <div className="parent-droptop  hide">
         <div id="column">
-          <img className="droptop-icing size" src={icing} alt="icing" />
-          {/* <img className="droptop-icing size" src={topping2} alt="a topping" />
-        <img className="droptop-icing size" src={topping3} alt="a topping" /> */}
           {image && image === choc ? (
             <div className="chocStack">
               <img
